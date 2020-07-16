25a07e58b9eb330f66d367dfafbbe920fefd93c6
Chris Yoon <chy.yoon@gmail.com>
2020-07-16 14:54:57 -0700

    pass the 1-9 test
    Co-authored-by: Kate Skorija <kateskorija@gmail.com>

--- NumbersToWords/Models/Translate.cs
+++ NumbersToWords/Models/Translate.cs
@@ -4,10 +4,23 @@ namespace NumbersToWords.Models
   public class Translate
   {
     public int Number { get; }
+    private static Dictionary<char, string> _oneToNineteen = new Dictionary<char, string>() { {'1', "one"}, {'2', "two"}, {'3', "one"}, {'4', "four"}, {'5', "five"}, {'6', "six"}, {'7', "seven"}, {'8', "eight"}, {'9', "nine"} };
 
     public Translate(int number)
     {
       Number = number;
+    }
+
+    public string TranslateNumber()
+    {
+      string result = "";
+      string numberString = Number.ToString();
+      char[] numberStringArray = numberString.ToCharArray();
+      foreach (char number in numberStringArray)
+      {
+      result = (_oneToNineteen[number]).ToString();
+      }
+      return result;
     }
   }
 }
@@ -16,8 +29,8 @@ namespace NumbersToWords.Models
 // for (int i = 0; i < array.length, i++)
 // if array.length = 2 and array[1] == 1 {one to Nineteen}
 
-// Dictionary<int, string> tensPlace = new Dictionary<int, string>{{2, "twenty"}, {3, "thirty"}, {4, "forty"}, {5, "fifty"}, {6, "sixty"}, {7, "seventy"}, {8, "eighty}, {9, "ninety"}}
+// Dictionary<char, string> tensPlace = new Dictionary<char, string>{{'2', "twenty"}, {'3', "thirty"}, {4, "forty"}, {5, "fifty"}, {6, "sixty"}, {7, "seventy"}, {8, "eighty}, {9, "ninety"}}
 // hundredths place - add " hundred and "
 // thousands place - add " thousand and "
 
-// Dictionary<int, string> oneToNineteen = new Dictionary<int, string>{{1, "one"},{2, "two"}{3, "one"}{4, "four"},{5, "five"}{6, "six"}{7, "seven"},{8, "eight"},{9, "nine"},{10, "ten"},{11, "eleven"}{12, "twelve"}{13, "thirteen"},{14, "fourteen"}{15, "fifteen"}{16, "sixteen"},{17, "seventeen"}{18, "eighteen"} {19, "nineteen"}}
+// {'10', "ten"}, {'11', "eleven"}, {'12', "twelve"}, {'13', "thirteen"}, {'14', "fourteen"}, {'15', "fifteen"}, {'16', "sixteen"}, {'17', "seventeen"}, {'18', "eighteen"}, {'19', "nineteen"}
